f567085f73e73b43a27fb679c9885d576ab19a1d
AKHIL TONGE <152062281+akhiltonge11@users.noreply.github.com>
2026-03-12 12:52:27 +0530

    Create Lead.java

---
+++ Lead.java
@@ -0,0 +1,72 @@
+package com.hsr.leadmanagement.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Lead {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String phone;
+    private String email;
+    private String carModel;
+    private String status;
+    private String source;
+
+    public Lead() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+}
